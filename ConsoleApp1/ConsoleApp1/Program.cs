455d53a48649586a8ba9638fa28b611fb8067404
ArithmeticOperations <edmon.grigoryan23@gmail.com>
2018-09-26 15:47:21 +0400

    inituial commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            floatOparation(10.45f, 25.98f);
+            ByteOparation(15, 25);
+
+            Console.ReadKey();
+        }
+
+        public static void floatOparation(float a, float b)
+        {
+            float c = a + b;
+            float k = a - b;
+            float d = a * b;
+
+            Console.WriteLine("a+b=" + c);
+            Console.WriteLine("a-b=" + k);
+            Console.WriteLine("a*b=" + d);
+
+            if (b != 0)
+            {
+                float f = a / b;
+                Console.WriteLine("a/b=" + f);
+            }
+            else
+            {
+                Console.WriteLine("You  cant devide by 0");
+            }
+        }
+
+
+        public static void ByteOparation(byte a, byte b)
+        {
+            int c = a + b;
+            int k = a - b;
+            int d = a * b;
+
+            Console.WriteLine("a+b=" + c);
+            Console.WriteLine("a-b=" + k);
+            Console.WriteLine("a*b=" + d);
+
+            if (b != 0)
+            {
+                float f =(float) a / b;
+                Console.WriteLine("a/b=" + f);
+            }
+            else
+            {
+                Console.WriteLine("You  cant devide by 0");
+            }
+        }
+
+    }
+}
---
+++ FirstStage/Types/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 15;
+            char sim = 'a';
+            string s = "aaa";
+            float k = 15.2015f;
+            double d = 125.45;
+        }
+    }
+}
