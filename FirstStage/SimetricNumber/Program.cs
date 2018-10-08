b3459e115aef20e5e5ef387a12f2309b11043bb3
ArithmeticOperations <edmon.grigoryan23@gmail.com>
2018-10-08 20:51:07 +0400

    Simetric number

---
+++ FirstStage/SimetricNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimetricNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int k = number;
+            int rev = 0;
+            while(k>0)
+            {
+                int s = k % 10;
+                rev = rev * 10 + s;
+                k = k / 10;
+            }
+            if (number==rev)
+            {
+                Console.WriteLine("Number  is  simetric");
+            }
+            else
+            {
+                Console.WriteLine("Number  is not  simetric");
+            }
+
+            
+        }
+    }
+}
---
+++ Test/Test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    string name = Console.ReadLine();
+            //    Console.WriteLine(name + "Hi");
+            //}
+
+            //for (int i = 0; i < 10; i++)
+            //{
+
+            //}
+            Console.WriteLine(  2+2+"2"+2);
+        }
+    }
+}
